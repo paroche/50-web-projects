cbf0238245c9d11cdfc18de13085b57e0d97d0e4
Peter Roche <sunmtnsft@aol.com>
2021-11-17 01:12:55 -0700

    carousel

---
+++ image-carousel/script.js
@@ -0,0 +1,47 @@
+/** @format */
+
+const imgs = document.getElementById('imgs');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+
+const img = document.querySelectorAll('#imgs img');
+
+let idx = 0;
+
+let interval;
+
+startSlide();
+
+function startSlide() {
+  if (interval) clearInterval(interval);
+  interval = setInterval(run, 2000);
+}
+
+function run() {
+  idx++;
+  changeImage();
+}
+
+function changeImage() {
+  if (idx < 0) idx = img.length - 1;
+  if (idx > img.length - 1) idx = 0;
+  imgs.style.transform = `translateX(${-idx * 500}px)`;
+}
+
+rightBtn.addEventListener('click', () => {
+  idx++;
+  changeImage();
+  startSlide();
+});
+
+leftBtn.addEventListener('click', () => {
+  idx--;
+  changeImage();
+  startSlide();
+});
+
+// His code. My version handles this in startSlide()
+function resetInterval() {
+  clearInterval(interval);
+  interval = setInterval(run, 2000);
+}
