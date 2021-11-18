f9f40aa24eebb99f5797b44b06d1d35a7fa60c1d
Peter Roche <sunmtnsft@aol.com>
2021-11-17 21:35:39 -0700

    hoverboard

---
+++ hoverboard/script.js
@@ -0,0 +1,32 @@
+/** @format */
+
+const container = document.getElementById('container');
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
+const SQUARES = 500;
+// const SQUARES = 4000;
+
+for (let i = 0; i < SQUARES; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+  square.addEventListener('mouseover', () => setColor(square));
+  square.addEventListener('mouseout', () => removeColor(square));
+  container.appendChild(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+  // element.style.boxShadow = `0 0 10px 1px ${color}`;
+  // element.style.boxShadow = `0 0 2px ${color}, 0 0 8px inset ${color}`;
+  // element.style.boxShadow = `0 0 4px inset ${color}`;
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = '#1d1d1d';
+  element.style.boxShadow = '0 0 2px #000';
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
