5a6257cade91469be78ba1ac0caea789aabc15ce
Peter Roche <sunmtnsft@aol.com>
2021-11-25 23:23:02 -0700

    Give misc-front-end-tiles random colors more alpha

--- 50-projects.js
+++ 50-projects.js
@@ -96,7 +96,7 @@ function randomColor() {
   const r = Math.floor(Math.random()*255);
   const g = Math.floor(Math.random()*255);
   const b = Math.floor(Math.random()*255);
-  const a = 0.2;
+  const a = 0.3;
   const color = `rgba(${r},${g},${b},${a})`;
   return color;
 }
