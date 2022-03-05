28c70ab6aae254fe100f971bac446d276136218a
Peter Roche <sunmtnsft@aol.com>
2022-03-05 15:42:52 -0700

    restore incrementing-counter, etc.

---
+++ incrementing-counter/script.js
@@ -0,0 +1,27 @@
+const counters = document.querySelectorAll('.counter');
+
+	counters.forEach(counter => {
+		counter.innerText = '0';
+		const updateCounter = () => {
+			const target = +counter.getAttribute('data-target');
+		const c = +counter.innerText;
+		const increment = target / 200;
+
+		// His version
+		// if(c < target) {
+		//   counter.innerText = `${Math.ceil(c + increment)}`;
+		//   setTimeout(updateCounter, 1);
+		// } else {
+		//   counter.innerText = target;
+		// }
+
+		// My version
+		if (c < target ) {
+			counter.innerText = `${Math.min(Math.ceil(c+increment),target)}`;
+			setTimeout(updateCounter, 1);
+		}
+		}
+
+		updateCounter();
+	})
+		
--- quote-generator/script.js
+++ quote-generator/script.js
@@ -28,18 +28,18 @@ function newQuote() {
   if (apiQuotes.length > 0) {
     quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     if (!quote) {
-      console.log('Using Local Quotes');
+      // console.log('Using Local Quotes');
       quote = newQuoteLocal();
     } else {
-      console.log('Using quote array from api');
+      // console.log('Using quote array from api');
     }
   } else {
-    console.log('Using Local Quotes, apiQuotes is null');
+    // console.log('Using Local Quotes, apiQuotes is null');
     // console.trace();
     quote = newQuoteLocal();
   }
   // Check if Author field is blank and replace it with "Unknown"
-  console.log(quote);
+  // console.log(quote);
   if (!quote.author) {
     authorText.textContent = 'Unknown';
   } else {
@@ -68,23 +68,23 @@ function newQuoteLocal() {
 
 // Get array of quotes from api
 async function getQuotes() {
-  console.log('in getQuotes..');
+  // console.log('in getQuotes..');
   // console.trace();
   showLoadingSpinner();
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     // throw new Error("testing error"); // testing
-    console.log('Fetching from ' + apiUrl);
+    // console.log('Fetching from ' + apiUrl);
     const response = await fetch(apiUrl);
-    console.log('Got api response...');
+    // console.log('Got api response...');
     apiQuotes = await response.json();
-    console.log('apiQuotes.length=' + apiQuotes.length);
+    // console.log('apiQuotes.length=' + apiQuotes.length);
     fetchErrCount = 0;
   } catch (error) {
-    console.log('Error on fetch: ' + error + ' fetchErrCount=' + fetchErrCount);
+    // console.log('Error on fetch: ' + error + ' fetchErrCount=' + fetchErrCount);
     fetchErrCount++;
     if (fetchErrCount < 10) {
-      console.log('Retrying api: ' + fetchErrCount);
+      // console.log('Retrying api: ' + fetchErrCount);
       getQuotes();
     }
   }
@@ -102,10 +102,10 @@ twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 async function onLoad() {
-  console.log('Loading...');
+  // console.log('Loading...');
   await getQuotes();
   newQuote();
-  console.log('Done loading.');
+  // console.log('Done loading.');
 }
 
 onLoad();
