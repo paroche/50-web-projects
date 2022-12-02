e7894ead04a84515e59e55f92b6b59522bdb6fef
Peter Roche <sunmtnsft@aol.com>
2022-12-02 00:07:07 -0700

    Add TV Search

---
+++ tv-search/script.js
@@ -0,0 +1,99 @@
+
+const TV_API_URL = `http://api.tvmaze.com/search/shows`
+const form = document.querySelector('#searchForm')
+const query = document.querySelector('#query');
+const submitButton = document.querySelector('#submit')
+const showsContainer = document.querySelector('#shows')
+
+form.addEventListener('submit', async function (e) {
+  e.preventDefault();
+  // console.dir(this)
+  const searchTerm = form.elements.query.value
+  const config = { params: {q: searchTerm}} // this is standard Axios for parameters instead of '?' after url. Can also have headers: {} as another entry in object (e.g. 'config' above
+  const res = await axios.get(TV_API_URL, config)
+  displayShows(res.data);
+  // form.elements.query.value = ''
+})
+
+// If press enter on query field, submit
+// query.addEventListener('keypress', function (event) {
+//   if (event.key === 'Enter') {
+//     event.preventDefault() // ?
+//     submitButton.click()
+//   }
+// })
+
+const displayShows = (shows) => {
+  for (let result of shows) {
+    const {averageRunTime, genres, image, name, network, premiered, ended, rating, runtime, schedule, status, summary, webChannel} = result.show;
+    const mediumImage = image? image.medium : makeImage(name)
+    const strippedSummary=stripped(summary)
+    const showEl = document.createElement('DIV')
+    showEl.classList.add('show')
+    let showHTML = `<img src="${mediumImage}" alt="${name}">
+      <div class="summary">
+        <div class="">
+          <h3>${name}</h3>
+        </div>
+        <div>
+          <h3>Summary</h3>
+            ${strippedSummary}
+        </div>
+        <h3><span class="heading">Genres:</span><span class="content">
+        `;
+    for (let genre of genres) {
+      showHTML += ` ${genre}, `
+      // console.log(genre)
+    }
+    showHTML = showHTML.slice(0, -2)
+    showHTML += `</span></h3>`
+    showHTML += `<h3><span class="heading">Premiered:</span><span class="content"> ${noNull(premiered)}</span></h3>`
+    if (ended) showHTML += `<h3><span class="heading">Ended:</span><span class="content"> ${noNull(ended)}</span></h3>`
+    if (webChannel) {
+      if (webChannel.name) {
+        showHTML += `<h3><span class="heading">Web Channel:</span><span class="content"> ${noNull(webChannel.name)} </span></h3>`
+      }}
+    showHTML += `</div>`
+    console.log(showHTML)
+    showEl.innerHTML = showHTML
+    showEl.addEventListener('click', () => showShow(result.show))
+    showsContainer.appendChild(showEl);
+  }
+}
+
+function noNull(string) {
+  if (string==null) return "" 
+  return string
+}
+
+function makeImage(name) {
+  return name
+}
+
+function showShow(show) {
+  showsContainer.innerHTML = ''
+  return null
+  console.log(show)
+  console.log(" ",show.name)
+  console.log("Genre(s):")
+  for (let genre of show.genres) {
+    console.log(" ",genre)
+  }
+  console.log(stripped(show.summary))
+  showIfNotNull("Premiered: ",show.premiered)
+  showIfNotNull("Ended:     ",show.ended)
+  if (show.webChannel) showIfNotNull("Web Channel: ",show.webChannel.name)
+  showIfNotNull("Average Run Time: ", show.averageRuntime)
+  showIfNotNull("Average Rating: ", show.rating.average)
+}
+
+
+
+function showIfNotNull(label, val) {
+  if (val) console.log(label, val)
+}
+
+function stripped(htmlString) {
+  let newString = htmlString.replace(/(<([^>]+)>)/gi, "").replace('&amp;', '&');
+  return newString
+}
